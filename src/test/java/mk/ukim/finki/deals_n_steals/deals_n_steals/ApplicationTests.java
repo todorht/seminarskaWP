0b72350b1a54335e219a2cd5730d17494e2527a1
todorht <hr.todorovski@gmail.com>
2021-01-29 14:52:30 +0100

    Starter

---
+++ src/test/java/mk/ukim/finki/deals_n_steals/deals_n_steals/ApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.deals_n_steals.deals_n_steals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
