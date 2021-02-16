9cb283095f03624497cfe6f4ccc1e34a1c0abb3d
KristijanTemelkovski <kristijan.temelkovski@students.finki.ukim.mk>
2021-02-16 16:58:19 +0100

    resources

---
+++ src/main/java/mk/ukim/finki/deals_n_steals/web/controller/HomeController.java
@@ -0,0 +1,15 @@
+package mk.ukim.finki.deals_n_steals.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping
+    public String home(){
+        return "home";
+    }
+}
