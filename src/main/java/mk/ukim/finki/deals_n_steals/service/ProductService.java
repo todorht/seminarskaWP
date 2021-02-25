859e5ec9097b366394887c2ee3e2e02bcd5f9ea4
Jordan <mihailjordanski@gmail.com>
2021-02-25 21:40:40 +0100

    javascript needed to be done

--- src/main/java/mk/ukim/finki/deals_n_steals/service/ProductService.java
+++ src/main/java/mk/ukim/finki/deals_n_steals/service/ProductService.java
@@ -1,20 +1,22 @@
 package mk.ukim.finki.deals_n_steals.service;
 
-import mk.ukim.finki.deals_n_steals.model.Category;
 import mk.ukim.finki.deals_n_steals.model.Product;
 import mk.ukim.finki.deals_n_steals.model.enumeration.Size;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
 public interface ProductService {
     List<Product> findAll();
+    List<Product> findAll(Pageable pageable);
     Product findById(Long id);
     List<Product> findAllBySuperCategory(String category);
+    List<Product> findAllBySuperCategory(String category, Pageable pageable);
     List<Product> findAllByCategory(String category);
+    List<Product> findAllByCategory(String category, Pageable pageable);
 
     Product save(String name, Size size, float price, String cat, String description, MultipartFile image) throws IOException;
     Product editProduct(Long id, String name, Size size, float price, String cat, String description, MultipartFile image) throws IOException;
--- src/main/java/mk/ukim/finki/deals_n_steals/web/controller/ProductController.java
+++ src/main/java/mk/ukim/finki/deals_n_steals/web/controller/ProductController.java
@@ -3,10 +3,13 @@ package mk.ukim.finki.deals_n_steals.web.controller;
 import mk.ukim.finki.deals_n_steals.model.Product;
 import mk.ukim.finki.deals_n_steals.model.ShoppingCart;
 import mk.ukim.finki.deals_n_steals.model.enumeration.CartStatus;
-import mk.ukim.finki.deals_n_steals.model.enumeration.OrderStatus;
 import mk.ukim.finki.deals_n_steals.model.enumeration.Size;
 import mk.ukim.finki.deals_n_steals.model.exception.ProductIsAlreadyInShoppingCartException;
+import mk.ukim.finki.deals_n_steals.model.SortClass;
 import mk.ukim.finki.deals_n_steals.service.*;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +17,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,6 +31,7 @@ public class ProductController {
     private final AuthService authService;
     private final EmailService emailService;
     private final OrderService orderService;
+    Pageable firstPage = PageRequest.of(0, 6);
 
     public ProductController(ProductService productService, CategoryService categoryService, ShoppingCartService shoppingCartService, AuthService authService, EmailService emailService, OrderService orderService) {
         this.productService = productService;
@@ -39,20 +42,42 @@ public class ProductController {
         this.orderService = orderService;
     }
 
-    @GetMapping({"","/{type}"})
+    @GetMapping({"","/{category}"})
     public String getProductsPage(@RequestParam(required = false) String error,
-                                  Model model,
                                   @RequestParam(required = false) String sort,
-                                  @PathVariable(required = false) String type,
-                                  @RequestParam(required = false) String state) {
-        List<Product> products = sorted(sort, type);
+                                  @PathVariable(required = false) String category,
+                                  @RequestParam(required = false) String state,
+                                  @RequestParam(required = false) Integer page,
+                                  Model model) {
         if(error!=null){
             model.addAttribute("hasError", true);
             model.addAttribute("error", error);
         }
 
+        if(sort == null){
+            model.addAttribute("sorting", "created-descending");
+            sort = "created-descending";
+        }
+        else
+            model.addAttribute("sorting", sort);
+
+        if(page == null){
+            page = 0;
+        }
+        model.addAttribute("page", page);
+
+        List<Product> products = sorted(sort, category, page);
+
         model.addAttribute("state",state);
 
+        addToModel(model, products);
+
+        temelkovkiTEST(model);
+
+        return "master-details";
+    }
+
+    private void temelkovkiTEST(Model model) {
         //test
         try  {
             ShoppingCart shoppingCart = this.shoppingCartService.findByUsernameAndStatus(this.authService.getCurrentUserId(), CartStatus.CREATED);
@@ -62,68 +87,6 @@ public class ProductController {
             model.addAttribute("size", 0);
 
         }//test
-
-        addToModel(model, products);
-
-        return "master-details";
-    }
-
-    private void addToModel(Model model, List<Product> products) {
-
-
-
-        model.addAttribute("ordersSize", this.orderService.findAllNewOrders().size());
-        model.addAttribute("products", products);
-        model.addAttribute("bodyContent", "products");
-        model.addAttribute("categories", this.categoryService.findAll());
-        model.addAttribute("tops", this.categoryService.findAllBySuperCategoryName("TOP"));
-        model.addAttribute("bottoms", this.categoryService.findAllBySuperCategoryName("BOTTOM"));
-        model.addAttribute("accessories", this.categoryService.findAllBySuperCategoryName("ACCESSORIES"));
-        model.addAttribute("collections", this.categoryService.findAllBySuperCategoryName("COLLECTIONS"));
-    }
-
-    private List<Product> sorted(String sort, String where) {
-        if(where == null)
-            where = "all";
-        List<Product> lista = switch (where) {
-            case "all" -> this.productService.findAll();
-            case "top" -> this.productService.findAllBySuperCategory("TOP");
-            case "bottom" -> this.productService.findAllBySuperCategory("BOTTOM");
-            case "accessories" -> this.productService.findAllBySuperCategory("ACCESSORIES");
-            case "collections" -> this.productService.findAllBySuperCategory("COLLECTIONS");
-            case "Shirts" -> this.productService.findAllByCategory("Shirts");
-            case "Jackets" -> this.productService.findAllByCategory("Jackets");
-            case "Hoodies" -> this.productService.findAllByCategory("Hoodies");
-            case "T-Shirts" -> this.productService.findAllByCategory("T-Shirts");
-            case "Knitwear" -> this.productService.findAllByCategory("Knitwear");
-            case "Blouses" -> this.productService.findAllByCategory("Blouses");
-            case "Tops" -> this.productService.findAllByCategory("Tops");
-            case "Trousers" -> this.productService.findAllByCategory("Trousers");
-            case "Jeans" -> this.productService.findAllByCategory("Jeans");
-            case "Skirts" -> this.productService.findAllByCategory("Skirts");
-            case "Earings" -> this.productService.findAllByCategory("Earings");
-            case "Bags" -> this.productService.findAllByCategory("Bags");
-            case "Dresses" -> this.productService.findAllByCategory("Dresses");
-            default -> this.productService.findAll();
-        };
-        if(sort == null) {
-            sort = "created-descending";
-        }
-        switch (sort){
-            case "price-ascending":
-                return lista.stream().sorted(Comparator.comparing(Product::getPrice)).collect(Collectors.toList());
-            case "price-descending":
-                return lista.stream().sorted(Comparator.comparing(Product::getPrice).reversed()).collect(Collectors.toList());
-            case "title-ascending":
-                return lista.stream().sorted(Comparator.comparing(Product::getName)).collect(Collectors.toList());
-            case "title-descending":
-                return lista.stream().sorted(Comparator.comparing(Product::getName).reversed()).collect(Collectors.toList());
-            case "created-descending":
-                return lista.stream().sorted(Comparator.comparing(Product::getCreateAt).reversed()).collect(Collectors.toList());
-            case "created-ascending":
-                return lista.stream().sorted(Comparator.comparing(Product::getCreateAt)).collect(Collectors.toList());
-            default: return lista;
-        }
     }
 
     @GetMapping("/add-product")
@@ -163,19 +126,6 @@ public class ProductController {
         }
         return "redirect:/products";
     }
-//
-//    @PostMapping("/{productId}")
-//    public String addProductToShoppingCart(HttpServletRequest request, @PathVariable Long productId) {
-//        String username = (String) request.getSession().getAttribute("username");
-//        try {
-//            this.shoppingCartService.addProductToShoppingCart(username, productId);
-//            return "redirect:/shopping-cart";
-//        }catch (ProductIsAlreadyInShoppingCartException ex){
-//            return "redirect:/products?error=" + ex.getMessage();
-//        }
-//
-//
-//    }
 
     @PostMapping("/delete/{id}")
     public String deleteProductWithPost(@PathVariable Long id) {
@@ -187,5 +137,91 @@ public class ProductController {
         return "redirect:/products";
     }
 
+    private void addToModel(Model model, List<Product> products) {
+        model.addAttribute("sortClass", napolni());
+        model.addAttribute("ordersSize", this.orderService.findAllNewOrders().size());
+        model.addAttribute("products", products);
+        model.addAttribute("bodyContent", "products");
+        model.addAttribute("categories", this.categoryService.findAll());
+        model.addAttribute("tops", this.categoryService.findAllBySuperCategoryName("TOP"));
+        model.addAttribute("bottoms", this.categoryService.findAllBySuperCategoryName("BOTTOM"));
+        model.addAttribute("accessories", this.categoryService.findAllBySuperCategoryName("ACCESSORIES"));
+        model.addAttribute("collections", this.categoryService.findAllBySuperCategoryName("COLLECTIONS"));
+    }
 
+    private List<Product> sorted(String sort, String category, Integer pagee) {
+        Pageable page = null;
+
+        switch (sort){
+            case "price-ascending":
+                page = PageRequest.of(pagee, 6, Sort.by("price")); break;
+            case "price-descending":
+                page = PageRequest.of(pagee, 6, Sort.by("price").descending()); break;
+            case "title-ascending":
+                page = PageRequest.of(pagee, 6, Sort.by("name")); break;
+            case "title-descending":
+                page = PageRequest.of(pagee, 6, Sort.by("name").descending()); break;
+            case "created-descending":
+                page = PageRequest.of(pagee, 6, Sort.by("createAt").descending()); break;
+            case "created-ascending":
+                page = PageRequest.of(pagee, 6, Sort.by("createAt"));
+        }
+
+        if(category == null)
+            category = "all";
+
+        List<Product> lista = null;
+
+        switch (category) {
+            case "all":
+                return this.productService.findAll(page);
+            case "top":
+                return this.productService.findAllBySuperCategory("TOP", page);
+            case "bottom":
+                return this.productService.findAllBySuperCategory("BOTTOM", page);
+            case "accessories":
+                return this.productService.findAllBySuperCategory("ACCESSORIES", page);
+            case "collections":
+                return this.productService.findAllBySuperCategory("COLLECTIONS", page);
+            case "Shirts":
+                return this.productService.findAllByCategory("Shirts", page);
+            case "Jackets":
+                return this.productService.findAllByCategory("Jackets", page);
+            case "Hoodies":
+                return this.productService.findAllByCategory("Hoodies", page);
+            case "T-Shirts":
+                return this.productService.findAllByCategory("T-Shirts", page);
+            case "Knitwear":
+                return this.productService.findAllByCategory("Knitwear", page);
+            case "Blouses":
+                return this.productService.findAllByCategory("Blouses", page);
+            case "Tops":
+                return this.productService.findAllByCategory("Tops", page);
+            case "Trousers":
+                return this.productService.findAllByCategory("Trousers", page);
+            case "Jeans":
+                return this.productService.findAllByCategory("Jeans", page);
+            case "Skirts":
+                return this.productService.findAllByCategory("Skirts", page);
+            case "Earings":
+                return this.productService.findAllByCategory("Earings", page);
+            case "Bags":
+                return this.productService.findAllByCategory("Bags", page);
+            case "Dresses":
+                return this.productService.findAllByCategory("Dresses", page);
+        }
+
+        return lista;
+    }
+
+    private List<SortClass> napolni() {
+        List<SortClass> lista = new ArrayList<>();
+        lista.add(new SortClass("Lowest Price", "price-ascending"));
+        lista.add(new SortClass("Highest Price", "price-descending"));
+        lista.add(new SortClass("Alphabetically, A-Z", "title-ascending"));
+        lista.add(new SortClass("Alphabetically, Z-A", "title-descending"));
+        lista.add(new SortClass("Date, New to Old", "created-descending"));
+        lista.add(new SortClass("Date, Old to New", "created-ascending"));
+        return lista;
+    }
 }
